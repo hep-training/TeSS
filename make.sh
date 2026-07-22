9dff67da1116ed176a272cdd50fa5d1230b826a3
kennethrioja <59597207+kennethrioja@users.noreply.github.com>
2026-07-22 15:33:43 +0200

    chore(misc): make the repo heptraining only

--- make.sh
+++ make.sh
@@ -4,8 +4,8 @@ set -euo pipefail
 # ------------------------------------------------------------------------------
 # make.sh — Build, push, and deploy TeSS
 # Usage:
-#   ./make.sh <trainingstg|training|eversetraining> <REGISTRY> <USERNAME> <REMOTE_REPO> <TAG>
-#   ./make.sh local <trainingstg|training|eversetraining>
+#   ./make.sh <trainingstg|training> <REGISTRY> <USERNAME> <REMOTE_REPO> <TAG>
+#   ./make.sh local <trainingstg|training>
 #   ./make.sh clean
 #   ./make.sh re
 # ------------------------------------------------------------------------------
@@ -22,8 +22,8 @@ NC="\033[0m"
 
 usage() {
   echo -e "${RED}[MAKE] Usage:${NC}"
-  echo "  ./make.sh <trainingstg|training|eversetraining> <REGISTRY> <USERNAME> <REMOTE_REPO> <TAG>"
-  echo "  ./make.sh local <trainingstg|training|eversetraining>"
+  echo "  ./make.sh <trainingstg|training> <REGISTRY> <USERNAME> <REMOTE_REPO> <TAG>"
+  echo "  ./make.sh local <trainingstg|training>"
   echo "  ./make.sh test <pathtotest>"
   echo "  ./make.sh clean"
   echo "  ./make.sh re"
@@ -136,7 +136,7 @@ main() {
   shift || true
 
   case "$CMD" in
-    trainingstg|training|eversetraining)
+    trainingstg|training)
       BUILD="$CMD"
       REGISTRY="${1:-}"
       USERNAME="${2:-}"
